74d39b86ca9ca270653a9fdf63879850fc767cdd
Ali Can <alibugraoglu@gmail.com>
2022-12-15 00:37:28 +0300

    Day 38 _OOP_Inheritance_MethodOverriding

--- src/Day38_OOP_InheritenceContinue_MethodOverriding/CarTask/BMW.java
+++ src/Day38_OOP_InheritenceContinue_MethodOverriding/CarTask/BMW.java
@@ -1,4 +1,15 @@
 package Day38_OOP_InheritenceContinue_MethodOverriding.CarTask;
 
-public class BMW {
+public class BMW extends Car{
+
+    public BMW(String model, int year, double price, String color, int miles) {
+        super("BMW", model, year, price, color, miles);
+    }
+
+    public void racing(){
+        System.out.println(brand + " " + model + " is racing in Dubai.");
+    }
+    public void breakdown (){
+        System.out.println(brand + " " + model + " breaks down every 100 miles.");
+    }
 }
--- src/Day38_OOP_InheritenceContinue_MethodOverriding/CarTask/Tesla.java
+++ src/Day38_OOP_InheritenceContinue_MethodOverriding/CarTask/Tesla.java
@@ -1,4 +1,13 @@
 package Day38_OOP_InheritenceContinue_MethodOverriding.CarTask;
 
-public class Tesla {
+public class Tesla extends Car {
+
+
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void autoPilot(){
+        System.out.println(brand + " " + model + " has autopilot function.");
+    }
 }
