b5a6c3b3e172b0527a3013ddb31aba19fc54691b
Ali Can <alibugraoglu@gmail.com>
2022-12-14 23:48:52 +0300

    Day 38 _OOP_Inheritance_MethodOverriding

--- src/Day38_OOP_InheritenceContinue_MethodOverriding/CarTask/Car.java
+++ src/Day38_OOP_InheritenceContinue_MethodOverriding/CarTask/Car.java
@@ -1,10 +1,48 @@
 package Day38_OOP_InheritenceContinue_MethodOverriding.CarTask;
 
 public class Car {
+    public String brand, model;
+    public int year;
+    public double price;
+    public String color;
+    public int miles;
 
 
+    public Car(String brand, String model, int year, double price, String color, int miles) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.color = color;
+        this.miles = miles;
+    }
 
+    public void start() {
+        System.out.println(brand + " " + model + " is staring");
 
+    }
+
+    public void drive() {
+        System.out.println(brand + " " + model + " is driving");
+
+    }
+
+    /*public void fly(){
+//  fly(): is not common for all the cars, and parent class should contain the common feature of child classes;
+    }
+
+     */
+
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", miles=" + miles +
+                '}';
+    }
 }
 /*
 carTask:
