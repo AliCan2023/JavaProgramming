baf3e2d3bde2d71078b0ded4bb478573488d8fa7
Ali Can <alibugraoglu@gmail.com>
2022-12-14 22:35:29 +0300

    day38:Inheritance- Method Overriding

---
+++ src/Day38_OOP_InheritenceContinue_MethodOverriding/CarTask/Car.java
@@ -0,0 +1,34 @@
+package Day38_OOP_InheritenceContinue_MethodOverriding.CarTask;
+
+public class Car {
+
+
+
+
+}
+/*
+carTask:
+		Create a class called Car
+			instance variables:
+				brand, model, year, price, color, miles
+
+				add a constructor to set all the fields
+
+			instance methods:
+				start(), drive(),toString()
+
+		Create the following sub classes of Car:
+			1. Toyota:
+					extra methods:
+							reliabile()
+
+			2. BMW:
+					extra methods:
+						breaksDown()
+						racing()
+
+			3. Tesla:
+					extra methods:
+						autoPilot();
+
+ */
